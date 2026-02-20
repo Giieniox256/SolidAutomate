56bc62c7f2d3520df01ca1d48a63c24c95b491c6
kowaliczek_256 <kowalik.piotr256@gmail.com>
2026-02-20 22:25:19 +0100

    func and findChilds

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SolidAutomate
 App for automate specific tasks in Solidworks
+
+# For Developers
+
+Run ypaf formating before merge
+
+```shell
+$ yapf -i -r .\src\solid_automate\
+```
--- src/solid_automate/gui/main_window.py
+++ src/solid_automate/gui/main_window.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QPushButton, QMessageBox
 
 
 class MainWindow(QMainWindow):
@@ -16,8 +16,47 @@ class MainWindow(QMainWindow):
         else:
             ui_main_file = Path(__file__).resolve().parent / "ui" / "solid_automate.ui"
 
+        # load ui from .ui
         self.ui_main = loader.load(ui_main_file)
         self.setCentralWidget(self.ui_main)
 
+        # setup tile
         self.setWindowTitle("SolidAutomate")
 
+        # assign widget objects with variables
+        self.btn_connect_solidW = self.ui_main.findChild(QPushButton, 'btn_connect_solid')
+        self.btn_disconnect_solidW = self.ui_main.findChild(QPushButton, 'btn_disconnect_solid')
+        self.btn_settings = self.ui_main.findChild(QPushButton, 'btn_settings')
+        self.btn_select_dir = self.ui_main.findChild(QPushButton, 'btn_select_dir')
+
+        try:
+            self.init_button_functions()
+        except Exception as e:
+            msg = QMessageBox(self)
+            msg.setIcon(QMessageBox.Icon.Critical)
+            msg.setWindowTitle("Error!")
+            msg.setText("Error while loading application. Some functionality may not work.")
+            msg.exec()
+
+    def init_button_functions(self):
+        # connect widgets with function
+        if self.btn_connect_solidW is not None:
+            self.btn_connect_solidW.clicked.connect(self.f_connect_solid)
+        if self.btn_disconnect_solidW is not None:
+            self.btn_disconnect_solidW.clicked.connect(self.f_disconnect_solid)
+        if self.btn_settings is not None:
+            self.btn_settings.clicked.connect(self.f_settings)
+        if self.btn_select_dir is not None:
+            self.btn_select_dir.clicked.connect(self.f_select_dir)
+
+    def f_connect_solid(self):
+        raise NotImplementedError("Connection not implemented")
+
+    def f_disconnect_solid(self):
+        raise NotImplementedError("Disconnection not implemented")
+
+    def f_settings(self):
+        raise NotImplementedError("Settings not implemented")
+
+    def f_select_dir(self):
+        raise NotImplementedError("Selection of Dir not implemented")
