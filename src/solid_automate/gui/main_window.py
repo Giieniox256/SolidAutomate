4981b1e32f3b9a02e38ac9f507df786433a2a2af
Giieniox256_MacOs <kowalik.piotr256@gmail.com>
2026-02-20 23:07:08 +0100

    prepare ui

--- src/solid_automate/gui/main_window.py
+++ src/solid_automate/gui/main_window.py
@@ -1,8 +1,11 @@
+"""
+main window class loading ui and connect function with manager
+"""
 import sys
 from pathlib import Path
 
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QMainWindow, QPushButton, QMessageBox
+from PySide6.QtWidgets import QMainWindow, QPushButton, QMessageBox, QProgressBar, QLabel, QTableWidget
 
 
 class MainWindow(QMainWindow):
@@ -28,6 +31,11 @@ class MainWindow(QMainWindow):
         self.btn_disconnect_solidW = self.ui_main.findChild(QPushButton, 'btn_disconnect_solid')
         self.btn_settings = self.ui_main.findChild(QPushButton, 'btn_settings')
         self.btn_select_dir = self.ui_main.findChild(QPushButton, 'btn_select_dir')
+        self.btn_start_job = self.ui_main.findChild(QPushButton, 'btn_start_job')
+        self.btn_stop_job = self.ui_main.findChild(QPushButton, 'btn_stop_job')
+        self.progress_bar = self.ui_main.findChild(QProgressBar, 'progress_bar')
+        self.lbl_actual_path = self.ui_main.findChild(QLabel, 'lbl_actual_path')
+        self.tb_main = self.ui_main.findChild(QTableWidget, 'tb_main')
 
         try:
             self.init_button_functions()
@@ -48,6 +56,12 @@ class MainWindow(QMainWindow):
             self.btn_settings.clicked.connect(self.f_settings)
         if self.btn_select_dir is not None:
             self.btn_select_dir.clicked.connect(self.f_select_dir)
+        if self.btn_start_job is not None:
+            self.btn_start_job.clicked.connect(self.f_start_job)
+        if self.btn_stop_job is not None:
+            self.btn_stop_job.clicked.connect(self.f_stop_job)
+        if self.progress_bar is not None:
+            self.progress_bar.setValue(0)
 
     def f_connect_solid(self):
         raise NotImplementedError("Connection not implemented")
@@ -60,3 +74,12 @@ class MainWindow(QMainWindow):
 
     def f_select_dir(self):
         raise NotImplementedError("Selection of Dir not implemented")
+
+    def f_start_job(self):
+        raise NotImplementedError("Starting job not implemented")
+
+    def f_stop_job(self):
+        raise NotImplementedError("Stopping job not implemented")
+
+    def f_set_label_actual_path(self, actual_path):
+        raise NotImplementedError("Setting label not implemented")
