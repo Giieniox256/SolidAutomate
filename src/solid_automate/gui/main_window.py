46e6cfb9a5b243695b14f12a6958385cc0f14a09
kowaliczek_256 <kowalik.piotr256@gmail.com>
2026-03-06 19:45:14 +0100

    add make dirs for docs to save

--- src/solid_automate/gui/main_window.py
+++ src/solid_automate/gui/main_window.py
@@ -6,6 +6,7 @@ import sys
 from pathlib import Path
 
 from PySide6.QtCore import QObject, Signal, Slot, QThread, Qt
+from PySide6.QtGui import QTextCursor
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import (
     QMainWindow,
@@ -14,9 +15,8 @@ from PySide6.QtWidgets import (
     QProgressBar,
     QLabel,
     QTableWidget, QFileDialog,
-    QTextBrowser,
+    QTextBrowser, QCheckBox,
 )
-from PySide6.QtGui import QTextCursor
 
 from solid_automate.core.solidworks_service import SolidWorksService
 
@@ -28,6 +28,7 @@ class MainWindow(QMainWindow):
     """Main window class"""
     request_connect = Signal()
     request_disconnect = Signal()
+    request_start_job = Signal(Path, list)
 
     def __init__(self):
         super().__init__()
@@ -52,6 +53,8 @@ class MainWindow(QMainWindow):
         self.worker.disconnected.connect(self.on_disconnected)
         self.request_connect.connect(self.worker.connect_solidworks)
         self.request_disconnect.connect(self.worker.disconnect_solidworks)
+        self.request_start_job.connect(self.worker.start_job)
+        self.worker.error_creating_dir.connect(self.on_error_creating_dir)
         self.thread.start()
 
         # setup tile
@@ -70,6 +73,9 @@ class MainWindow(QMainWindow):
         self.status_label = self.ui_main.findChild(QLabel, 'status_label')
         self.tb_console = self.ui_main.findChild(QTextBrowser, 'tb_console')
         self.console_manager = TextBrowserManager(self.tb_console)
+        self.cb_dxf = self.ui_main.findChild(QCheckBox, 'check_box_dxf')
+        self.cb_pdf = self.ui_main.findChild(QCheckBox, 'check_box_pdf')
+        self.cb_step = self.ui_main.findChild(QCheckBox, 'check_box_step')
 
         try:
             self.init_button_functions()
@@ -96,6 +102,14 @@ class MainWindow(QMainWindow):
         self.status_label.setText("Fail. Try to reconnect.")
         self.console_manager.insert_text(f"Error occurred. {error_msg}")
         self.btn_connect_solidworks.setEnabled(True)
+
+    def on_error_creating_dir(self, error_msg):
+        """Function called when error_msg occurs"""
+        self.console_manager.insert_text(f"Error occurred while creating dir's. {error_msg}")
+
+    def get_files_to_produce(self) -> list:
+        """Function called when get_files_to_produce is called. Return state of checkbox's"""
+        return [self.cb_pdf.checkState(), self.cb_step.checkState(), self.cb_dxf.checkState()]
 
     def init_button_functions(self) -> None:
         """Functions connect a button object with functions"""
@@ -140,7 +154,7 @@ class MainWindow(QMainWindow):
 
     def f_start_job(self) -> None:
         """Function start job: selected file will be generated"""
-        raise NotImplementedError("Starting job not implemented")
+        self.request_start_job.emit(self.selected_dir, self.get_files_to_produce())
 
     def f_stop_job(self) -> None:
         """Function stop actual preparing docs job"""
@@ -156,8 +170,9 @@ class SolidWorker(QObject):
     disconnected = Signal()
     saved = Signal()
     settings = Signal()
-    select_dir = Signal()
+    select_dir = Signal(str)
     error = Signal(str)
+    error_creating_dir = Signal(str)
     message = Signal()
     job_progress = Signal(int, int)
     job_done = Signal(int)
@@ -166,6 +181,7 @@ class SolidWorker(QObject):
     def __init__(self):
         super().__init__()
         self.sw = SolidWorksService()
+        self.proj_path = None
 
     @Slot()
     def connect_solidworks(self):
@@ -182,13 +198,39 @@ class SolidWorker(QObject):
         self.sw.shutdown()
         self.disconnected.emit()
 
-    @Slot()
+    @staticmethod
+    def check_doc_dir_exist(path) -> bool:
+        """Function check if doc dir exist"""
+        ddir = ["DXF", "PDF", "STEP"]
+        doc_pth = Path(path) / "Dokumentacja"
+        Path(doc_pth).mkdir(parents=True, exist_ok=True)
+        for d in ddir:
+            sub_dir = doc_pth / d
+            Path(sub_dir).mkdir(parents=True, exist_ok=True)
+        if not doc_pth.exists():
+            return False
+        return True
+
     def open_part(self, path):
         try:
             result = self.sw.open_part(path)
             self.message.emit(result)
         except Exception as e:
             self.error.emit(e)
+
+    @Slot()
+    def start_job(self, path: Path, types_to_produce: list):
+        """Function start job: selected file type will be generated"""
+        if not self.check_doc_dir_exist(path):
+            self.error.emit(f"Document dir not created: {path}")
+            return
+        _pdf, _step, _dxf = types_to_produce
+        if _pdf == Qt.CheckState.Checked:
+            print("make PDF")
+        if _step == Qt.CheckState.Checked:
+            print("make Step")
+        if _dxf == Qt.CheckState.Checked:
+            print("make DXF")
 
 
 class TextBrowserManager:
