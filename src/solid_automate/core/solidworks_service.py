77c664ac1f0e1b1ce1e604924cd96cf8e7d42f29
kowaliczek_256 <kowalik.piotr256@gmail.com>
2026-09-01 22:52:37 +0200

    update broken step save

--- src/solid_automate/core/solidworks_service.py
+++ src/solid_automate/core/solidworks_service.py
@@ -86,7 +86,7 @@ class SolidWorksService:
 
     def save_part_to_step(self, file_path=None, file_name=None):
         if self.swModel.GetType == 1 or self.swModel.GetType == 2:
-            arg3 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, 1)
+            arg3 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, 0)
             arg4 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, 2)
             success = self.swModel.SaveAs3(f"{file_path}\\{file_name}.STEP", arg3, arg4)
             return True if success == 0 else False
